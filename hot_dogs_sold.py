1a3e70f6f24e835adedd08908235e7de98fecba4
jaycebaxter <jayce.baxter.9@gmail.com>
2024-10-01 20:19:18 -0400

    Comments only

---
+++ hot_dogs_sold.py
@@ -0,0 +1,10 @@
+##########################################################################
+
+# Program: Hot Dog Total Calculator
+# Author: Jayce Johnson
+# Date: October 1st, 2024
+# Description: Lets the user tally up hot dogs as they are sold, outputting
+#              a total number of each hot dog sold, as well as percentage.
+
+##########################################################################
+
